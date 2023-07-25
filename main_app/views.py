1d3e6a2c94d9c4e6bfc3c72d14850247b870af95
janicavc <crisostomo_janica@yahoo.com>
2023-07-25 15:12:51 -0700

    initial commit

---
+++ main_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+sonnyangels = [
+    {'name': 'Shark' , 'series': 'Marine' , 'description': 'Classic marine animal!' }
+    {'name': 'Chipmunk' , 'series': 'Animal Series 1' , 'description': 'Rare and cute!' }
+]
+
+
+
