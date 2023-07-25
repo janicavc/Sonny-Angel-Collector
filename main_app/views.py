b15a6bdc6c2212dc2f7aa6a57789ecba1d88973d
janicavc <crisostomo_janica@yahoo.com>
2023-07-25 15:41:53 -0700

    added about and view in nav

--- main_app/views.py
+++ main_app/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 sonnyangels = [
-    {'name': 'Shark' , 'series': 'Marine' , 'description': 'Classic marine animal!' }
+    {'name': 'Shark' , 'series': 'Marine' , 'description': 'Classic marine animal!' },
     {'name': 'Chipmunk' , 'series': 'Animal Series 1' , 'description': 'Rare and cute!' }
 ]
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
 
 
 
