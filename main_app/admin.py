9ac1ccbfdd8483b05b54844d630771058b3518a2
janicavc <crisostomo_janica@yahoo.com>
2023-07-27 16:04:54 -0700

    added inspection feature

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Sonny
+from .models import Sonny, Inspect
 
 # Register your models here.
 admin.site.register(Sonny)
+admin.site.register(Inspect)
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+CONDITIONS = (
+    ('N', 'Like New'),
+    ('S', 'Slightly Damaged'),
+    ('D', 'Damaged')
+)
+
 # Create your models here.
 class Sonny(models.Model):
     name = models.CharField(max_length=75)
@@ -12,3 +18,16 @@ class Sonny(models.Model):
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'sonnyangel_id': self.id})
+    
+
+class Inspect(models.Model):
+    date = models.DateField('Inspect Date')
+    condition = models.CharField(
+        max_length=1,
+        choices=CONDITIONS,
+        default=CONDITIONS[0][0]
+    )
+    sonnyangel = models.ForeignKey(Sonny, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_condition_display()} on {self.date}"
